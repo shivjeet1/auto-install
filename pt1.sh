3925a4d3bdb61c11e3ab4b675396b9e4765bc53b
shivjeet1 <shivamlavhalepatil@gmail.com>
2025-03-02 14:10:56 +0530

    fakjsdsdhjfkl

--- pt1.sh
+++ pt1.sh
@@ -1,35 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 echo -e "Starting Installation Script by @shivjeet1"
 read -p "\nNote: Have you partitioned the DISK ? [y/n]" yn
-if [ $yn != "y"]
-then
+if [[ $yn != "y"]]; then
 	echo -e "Perform partitioning with [fdisk/gparted] then run this script again."
 	exit 1
 fi
 
 read -p "EFI Partition: " pefi
 echo "Formatting efi partition to fat32"
-mkfs.fat -F 32 $pefi
+mkfs.fat -F 32 "$pefi"
 
 read -p "Root Partition: " proot
 echo "Formatting Root Partition to ext4"
-mkfs.ext4 $proot
+mkfs.ext4 "$proot"
 
 read -p "Do you need SWAP ?[y/n]" syn
-if [ $syn == "y" ]
-then
+if [[ $syn == "y" ]];  then
 	read -p "SWAP Partition: " swapp
 	echo "Creating SWAP"
-	mkswap $swapp
+	mkswap "$swapp"
 	echo "Mounting SWAP"
-	swapon $swapp
+	swapon "$swapp"
 fi
 
 echo "Mounting Root"
-mount $proot /mnt
+mount "$proot" /mnt
 
 echo "Mounting EFI"
-mount --mkdir $pefi /mnt/boot
+mount --mkdir "$pefi" /mnt/boot
 
 echo "Installing Packages"
 pacstrap -K /mnt base linux linux-firmware grub efibootmgr neovim networkmanager sudo openssh intel-ucode man-db zsh git mesa || exit 1
@@ -38,7 +36,17 @@ echo "Generating fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
 echo "Chrooting into environment"
-arch-chroot /mnt
+arch-chroot /mnt << EOF
+
+echo "Setting up keyboard"
+echo "LANG=en_IN.UTF-8" > /etc/locale.conf
+
+echo "Setting up Timezone[INDIA]"
+ln -sf  /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+hwclock --systohc
+
+passwd
+EOF
 
 echo "Setting up keyboard"
 echo "LANG=en_IN.UTF-8" > /etc/locale.conf
@@ -49,21 +57,21 @@ hwclock --systohc
 echo "Setup password for ROOT user : "
 passwd
 
-read -p "Creating New user"
-useradd -m -G wheel -s /bin/bash $user
+read -p "Creating New user:" user
+useradd -m -G wheel -s /bin/bash "$user"
 echo "Setting password for $user"
 passwd $user
 echo '%wheel ALL=(ALL:ALL) ALL' | sudo EDITOR='tee -a' visudo
 chsh -s /bin/zsh $user
 
 read -p "Enter HostName : " hname
-echo $hname > /etc/hostname
+echo "$hname" > /etc/hostname
 echo -e "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.0.1.1\tlocalhost.localdomain\t$hname" >> /etc/hosts
 systemctl enable NetworkManager
 
 echo "Setting-up Bootloader"
 sed -i /etc/default/grub '/PROBER\=/s/\#//'
-p_efi = $(df | grep /boot | awk '{ print $1 }')
+p_efi = $(df | grep /boot | awk '{print $1}')
 grub-install ${p_efi::-1}
 grub-mkconfig -o /boot/grub/grub.cfg
 
