97edde11d78b73df04904400447580b2738fd98c
shivjeet1 <shivamlavhalepatil@gmail.com>
2025-03-03 17:36:36 +0530

    jfals

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ echo "."
 export DISPLAY=3
 
 source $HOME/.zprofile 2> /dev/null
-wall -s -i $HOME/auto-install/sung-jinwoo-amoled-5120x2880-15859.jpg
+wal -s -i $HOME/auto-install/sung-jinwoo-amoled-5120x2880-15859.jpg
 sed '/urg/d' -i $XDG_CACHE_HOME/wal/colors-wal-dwm.h
 sed '31s/0/256/' -i $XDG_CACHE_HOME/wal/colors-wal-st.h
 
